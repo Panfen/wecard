6542c64ff45b043cbe70b5470607d7fe2e074358
Panfen <2809956575@qq.com>
2019-05-24 17:27:10 +0800

    add_card_list

--- miniprogram/pages/circle/circle.js
+++ miniprogram/pages/circle/circle.js
@@ -1,46 +1,40 @@
 // miniprogram/pages/circle/circle.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    cardList: []
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
-
+    // this.getCardList()
+    wx.cloud.callFunction({
+      name: 'getcardlist',
+      data: {},
+      success: res => {
+        this.setData({
+          cardList: res.result.data
+        })
+      }, 
+      fail: err => {
+        console.error('[数据库] [云函数查询记录] 失败：', err)
+      }
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
+  getCardList: function () {
+    const db = wx.cloud.database()
+    db.collection('cards').where({
+      _ispublic: 1
+    }).get({
+      success: res => {
+        console.log(JSON.stringify(res.data, null, 2))
+        this.setData({
+          cardList: res.data
+        })
+      },
+      fail: err => {
+        console.error('[数据库] [查询记录] 失败：', err)
+      }
+    })
   },
 
   /**
